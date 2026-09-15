e5e29eb3d199b1643aeaeecdcbffb45039a3175b
Chikondi Kamwendo <chikondikamwendo@yahoo.com>
2026-09-15 13:53:30 +0200

    Add initial tests for user login and set up Pest testing framework
    - Created LoginTest.php to outline user login functionality with todos for validation checks.
    - Introduced Pest.php to extend the TestCase class and set up custom expectations and helper functions for testing.

---
+++ api/tests/Feature/Auth/LoginTest.php
@@ -0,0 +1,9 @@
+<?php
+
+todo("user can login");
+
+describe("validation", function () {
+    todo("requires email");
+    todo("requires correct email");
+    todo("requires correct password");
+});
