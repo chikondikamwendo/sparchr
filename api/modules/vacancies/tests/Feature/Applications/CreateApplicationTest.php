77d63447ef1aee251cc2486d14d6e22744f78a6e
Chikondi Kamwendo <chikondikamwendo@yahoo.com>
2026-09-18 20:02:49 +0200

    feat (tests): add CreateApplicationTest with validation and pipeline scenarios

---
+++ api/modules/vacancies/tests/Feature/Applications/CreateApplicationTest.php
@@ -0,0 +1,20 @@
+<?php
+
+todo('creates an application to a vacancy');
+
+describe('validation', function () {
+    todo('requires name');
+    todo('requires email');
+    todo('requires email to be unique');
+    todo('requires gender');
+    todo('requires date of birth');
+    todo('requires bio');
+    todo('requires experiences');
+    todo('requires qualifications');
+    todo('rejects late submission');
+});
+
+describe('pipeline', function () {
+    todo('sends email to acknowledge receipt');
+    todo('scores application');
+});
